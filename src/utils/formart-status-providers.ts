c20d37c31df02fece0c7538eaf8397d95d089ce8
Renildo15 <renildorabi@ufrn.edu.br>
2026-03-03 20:10:39 -0300

    feat: funções auxiliares

---
+++ src/utils/formart-status-providers.ts
@@ -0,0 +1,12 @@
+export function formatProviderStatus(status: string) {
+    switch (status) {
+        case "pending":
+            return "Pendente"
+        case "approved":
+            return "Aprovado"
+        case "rejected":
+            return "Rejeitado"
+        default:
+            return status
+    }
+}
---
+++ src/utils/format-categories.ts
@@ -0,0 +1,5 @@
+import { Category } from "../types/category";
+
+export function formatCategories(categories: Category[]) {
+    return categories.map(cat => cat.name).join(', ')
+}
