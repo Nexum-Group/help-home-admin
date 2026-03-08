a7aecc4332ded980632b4fab1a0536eb14ac0db6
Renildo15 <renildorabi@ufrn.edu.br>
2026-03-08 16:23:52 -0300

    feat:tipagem

--- src/types/user.ts
+++ src/types/user.ts
@@ -1,5 +1,7 @@
 import { Address } from "./address";
 import { Provider } from "./provider";
+import { Review } from "./review";
+import { ServiceRequest } from "./service";
 
 export type User = {
     id: string;
@@ -18,4 +20,10 @@ export type User = {
 
 export type AllUsers = User & {
     provider_profile: Provider | null;
+}
+
+export type UserDetail = User & {
+    services: ServiceRequest[];
+    reviews: Review[];
+    profile_provider: Provider | null
 }
